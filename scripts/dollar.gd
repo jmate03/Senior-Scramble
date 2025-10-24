5b758d990c7fa1b9dbb760f22c76e46d5023e039
jmate03 <jsmateo@email.sc.edu>
2025-10-24 17:28:58 -0400

    minor debug
    duplicate connection. game still ran but console was flooded with messages

--- scripts/dollar.gd
+++ scripts/dollar.gd
@@ -5,7 +5,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
